b7825b5784dc0bdca670a066adf9903ffb44191b
Julie Victoria Kongsten <julievk@stud.ntnu.no>
2021-06-30 12:18:18 +0200

    added props consept to the expense item. The data is now stored inn App.js

--- 01-starting-setup/src/components/ExpenseItem.js
+++ 01-starting-setup/src/components/ExpenseItem.js
@@ -1,17 +1,14 @@
 import "./ExpenseItem.css";
 
-
-function ExpenseItem() {
-  const expenseDate = new Date(2021, 2, 28);
-  const expenseTitle = "Car Insurance";
-  const expenseAmount = 294.67;
-
+//props is used to describe tho whole object we are getting
+//props consist of key-value pairs
+function ExpenseItem(props) {
   return (
     <div className="expense-item">
-      <div>{expenseDate.toISOString()}</div>
+      <div>{props.date.toISOString()}</div>
       <div className="expense-item__description">
-        <h2>{expenseTitle}</h2>
-        <div className="expense-item__price">${expenseAmount}</div>
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
       </div>
     </div>
   );
