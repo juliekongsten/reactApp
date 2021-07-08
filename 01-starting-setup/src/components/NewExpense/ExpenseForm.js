b857073aad7c0c88afe2a50c12fb9979c1627b6b
Julie Victoria Kongsten <julievk@stud.ntnu.no>
2021-07-08 11:33:45 +0200

    Made a form so that you can soon add your own expenses

---
+++ 01-starting-setup/src/components/NewExpense/ExpenseForm.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ExpenseForm from "./ExpenseForm.css";
+
+const ExpenseForm = () =>{};
+
+export default ExpenseForm;
---
+++ 01-starting-setup/src/components/NewExpense/NewExpense.js
@@ -0,0 +1,12 @@
+import React from "react";
+import NewExpense from "./NewExpense.css";
+
+
+//i want a form for the input so you can add an expense to the tracker
+const NewExpense = () => {
+    return <div className="new-expense">
+        <form></form>
+    </div>
+};
+
+export default NewExpense;
